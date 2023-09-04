19ff62cf12ec61ab0208dabef561365f20990805
ransubloom92 <114718107+kimransu@users.noreply.github.com>
2023-09-04 16:38:17 +0900

    졸전1차심사 업로드 0904

---
+++ src/subject2/script/reference.js
@@ -0,0 +1,9 @@
+const reference = [
+  {
+    author: '남인숙',
+    title: '사실, 내성적인 사람입니다',
+    year: '2019',
+    sourceTitle: '21세기북스',
+    link: 'http://www.book21.com/book/book_view.html?bookSID=4940',
+  },
+];
